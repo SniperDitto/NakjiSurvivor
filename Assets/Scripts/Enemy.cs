1fb47d9edba3b6c3954146505838d26c23b02069
mon <sniperditto@kakao.com>
2025-01-16 23:46:13 +0900

    add dead animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,19 +14,23 @@ public class Enemy : MonoBehaviour
 
     private bool _isAlive;
     Rigidbody2D _rigidbody;
+    private Collider2D _collider;
     SpriteRenderer _spriteRenderer;
     Animator _animator;
+    private WaitForFixedUpdate _waitForFixedUpdate;
     
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider =  GetComponent<Collider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _waitForFixedUpdate = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!_isAlive) return;
+        if (!_isAlive || _animator.GetCurrentAnimatorStateInfo(0).IsName("Hit") ) return;
         Vector2 directionVector = target.position - _rigidbody.position;
         Vector2 nextVector = directionVector.normalized * (speed * Time.fixedDeltaTime);
         _rigidbody.MovePosition(_rigidbody.position + nextVector);
@@ -44,7 +48,11 @@ public class Enemy : MonoBehaviour
         _isAlive = true;
         target = GameManager.Instance.player.GetComponent<Rigidbody2D>();
         health = maxHealth;
-        
+        _isAlive = true;
+        _collider.enabled = true;
+        _rigidbody.simulated = true;
+        _spriteRenderer.sortingOrder = 2;
+        _animator.SetBool("Dead", false);
     }
 
     public void Init(SpawnData data)
@@ -60,16 +68,33 @@ public class Enemy : MonoBehaviour
         if (!other.CompareTag("Bullet")) return;
 
         health -= other.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health > 0)
         {
             // hit
+            _animator.SetTrigger("Hit");
         }
         else
         {
             // dead
-            Dead();
+            _isAlive = false;
+            _collider.enabled = false;
+            _rigidbody.simulated = false;
+            _spriteRenderer.sortingOrder = 1; // 캐릭터 가리지 않기 위해 순서 변경
+            _animator.SetBool("Dead", true);
+            
         }
+    }
+
+    IEnumerator KnockBack()
+    {
+        var knockBackSize = 3;
+        //yield return null; // 1프레임 쉬기
+        yield return _waitForFixedUpdate; // 다음 하나의 물리 프레임 딜레이
+        Vector3 playerPos = GameManager.Instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        _rigidbody.AddForce(dirVec.normalized * knockBackSize, ForceMode2D.Impulse);
     }
 
     void Dead()
