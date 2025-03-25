18d1ccb6ed63756431c93a1f1e1838133ded13ee
you_sunghwa <you_sunghwa@coconev.co.jp>
2025-03-25 14:01:01 +0900

    add input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -23,15 +24,9 @@ public class Player : MonoBehaviour
         scanner = GetComponent<Scanner>();
     }
 
-    void Update()
-    {
-        inputVector.x = Input.GetAxisRaw("Horizontal");
-        inputVector.y = Input.GetAxisRaw("Vertical");
-    }
-
     void FixedUpdate()
     {
-        Vector2 nextVector = inputVector.normalized * (speed * Time.fixedDeltaTime);
+        Vector2 nextVector = inputVector * (speed * Time.fixedDeltaTime);
         //_rigidbody2D.AddForce(inputVector); //힘을 가하기
         //_rigidbody2D.velocity = inputVector; //속도를 바꾸기
         _rigidbody2D.MovePosition(_rigidbody2D.position + nextVector); //위치를 바꾸기
@@ -46,5 +41,10 @@ public class Player : MonoBehaviour
         {
             _spriteRenderer.flipX = inputVector.x < 0;
         }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
     }
 }
